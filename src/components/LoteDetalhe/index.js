5716daaaa46bf35a7446c8b3039c56a3c770c55d
Gleidson Pinheiro <ext.accurate.gleidson.pereira@gympass.com>
2021-08-13 22:35:20 -0300

    initial commit

---
+++ src/components/LoteDetalhe/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Style } from './style'
+
+const LoteDetalhe = ({ text }) => {
+
+  return (
+
+    <Style>
+      <h4>{text}</h4>
+      <button>Click</button>
+    </Style>
+  )
+}
+
+export default LoteDetalhe;
---
+++ src/components/LoteDetalhe/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Style = styled.div`
+    width:  300px;
+    height: 300px;
+    background: grey;
+`;
